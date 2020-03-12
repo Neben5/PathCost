c5d5babdb907f52d9d55617f7ce62e71eb81e425
Benjamin <jlsatpa@gmail.com>
2020-03-11 20:34:31 -0700

    yo

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 package src;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String... args) throws Exception {
         new Main();
     }
+
     private int target;
+
     Main() throws Exception {
         int[][] lines = Reader.read();
         target = lines[0][1];
+        Edge[] edges = new Edge[lines.length - 1];
+        for (int i = 1; i < lines.length; i++) {
+            edges[i-1] = new Edge(lines[i][0], lines[i][1], lines[i][2]);
+        }
+
+
     }
 }
